183cf644de665f212d27cb6e5e9d9edf42405e89
Vladyslav Bronevych <vbronevy@c3r6s1.42warsaw.pl>
2025-02-06 18:03:24 +0100

    Changes to be committed: 	modified:   a.out 	modified:   ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -15,139 +15,6 @@
 #include <string.h>
 #include "libft/libft.h"
 
-// int max_of(int count, ...)
-// {
-// 	va_list args;
-// 	int		num;
-// 	int		n;
-// 	int		i;
-
-// 	va_start(args, count);
-// 	num = va_arg(args, int);
-// 	i = 0;
-// 	while(i < count)
-// 	{
-// 		n = va_arg(args, int);
-// 		if (num < n)
-// 		{
-// 			num = n;
-// 		}
-// 		i++;
-// 	}
-
-// 	va_end(args);
-
-// 	return (num);
-// }
-
-// float average_of(int count, ...)
-// {
-// 	va_list args;
-// 	int		i;
-// 	float	num;
-
-// 	va_start(args, count);
-// 	i = 0;
-// 	num = 0;
-// 	while(i < count)
-// 	{
-// 		num = num + va_arg(args, double);
-// 		i++;
-// 	}
-// 	num = num / count;
-// 	va_end(args);
-
-// 	return (num);
-// }
-
-// char* concat_strings(int count, ...)
-// {
-// 	va_list args;
-// 	char*	str;
-// 	int 	i;
-	
-// 	va_start(args, count);
-// 	i = 0;
-// 	str = va_arg(args, char*);
-// 	while(i < count - 1)
-// 	{
-// 		str = ft_strjoin(str, va_arg(args, char*));
-// 		i++;
-// 	}
-// 	va_end(args);
-	
-// 	return(str);
-// }
-
-// int product_of(int count, ...)
-// {
-// 	va_list	args;
-// 	int		i;
-// 	int		sum;
-	
-// 	va_start(args, count);
-// 	i = 0;
-// 	sum = 1;
-// 	while(i < count)
-// 	{
-// 		sum = sum * va_arg(args, int);
-// 		i++;
-// 	}
-// 	va_end(args);
-// 	return(sum);
-// }
-
-// void print_mixed(int count, ...)
-// {
-// 	va_list args;
-// 	int		i;
-// 	int		type;
-	
-// 	va_start(args, count);
-// 	i = 0;
-// 	while(i < count)
-// 	{
-// 		type = va_arg(args, int);
-// 		if(type == 'i')
-// 		{
-// 			printf("Integer: %d\n", va_arg(args, int));
-// 		}
-// 		if(type == 'c')
-// 		{
-// 			char c = va_arg(args, int);
-// 			printf("Character: %c\n", c);
-// 		}
-// 		if(type == 'f')
-// 		{
-// 			printf("Float: %f\n", va_arg(args, double));
-// 		}
-// 		i++;
-// 	}
-// 	va_end(args);
-// }
-
-// void reverse_print(int count, ...)
-// {
-// 	va_list args;
-// 	int		i;
-// 	int		arr[count];
-	
-// 	va_start(args,count);
-// 	i = 0;
-// 	while(i < count)
-// 	{
-// 		arr[i] = va_arg(args, int);
-// 		i++;
-// 	}
-// 	while(i > 0)
-// 	{
-// 		i--;
-// 		printf("%d ", arr[i]);
-// 	}
-// 	printf("\n");
-// 	va_end(args);
-// }
-
 int	count_args(const char* str)
 {
 	int	i;
@@ -155,7 +22,7 @@ int	count_args(const char* str)
 
 	i = 0;
 	sum = 0;
-	while(str[i])
+	while (str[i])
 	{
 		if(str[i] == '%')
 		{
@@ -164,7 +31,7 @@ int	count_args(const char* str)
 		i++;
 	}
 	return(sum);
-}
+}	
 
 char	get_specifier(const char* str, int start)
 {
@@ -174,56 +41,83 @@ char	get_specifier(const char* str, int start)
 	
 	count = 0;
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
-		if(str[i] == '%')
+		if (str[i] == '%')
 		{
 			count++;
-			if(count <= start && start > 0)
+			if (count <= start && start > 0)
 			{
 				i++;
 				continue;
 			}
-			if(str[i + 1] == 's')
+			if (str[i + 1] == 's')
 				specifier = 's';
-			else if(str[i + 1] == 'i')
+			else if (str[i + 1] == 'i')
 				specifier = 'i';
 			break;
 		}
 		i++;
 	}
-	return(specifier);
+	return (specifier);
 }
 
-char*	get_result(const char* format, char* str)
+char*	put_string(const char* format, char* str)
 {
 	int		i;
 	char*	copy;
 	char*	result;
+	char*	start;
 
 	i = 0;
-	result = malloc(ft_strlen(format) + ft_strlen(str) + 1);
-	copy = (char*)format;
-	while(format[i])
+	while (format[i])
 	{
-		result[i] = format[i];
-		if(format[i] == '%')
+		if (format[i] == '%')
 		{
-			result[i] = '\0';
-			result = ft_strjoin(result, str);
-			result = ft_strjoin(result, format + i + 2);
+			start = ft_substr(format, 0, i);
+			copy = ft_strjoin(start, str);
+			result = ft_strjoin(copy, format + i + 2);
+			free(copy);
+			free(start);
 			break;
 		}
 		i++;
 	}
-	return(result);
+	return (result);
 }
+char	*put_number(const char* format, int num)
+{
+	int		i;
+	char*	copy;
+	char*	result;
+	char*	start;
+	char*	arg;
+
+	i = 0;
+	arg = ft_itoa(num);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			start = ft_substr(format, 0, i);
+			copy = ft_strjoin(start, arg);
+			result = ft_strjoin(copy, format + i + 2);
+			free(arg);
+			free(copy);
+			free(start);
+			break;
+		}
+		i++;
+	}
+	return (result);
+}
+
 void print_result(const char* result)
 {
 	int	i;
 
 	i = 0;
-	while(result[i])
+	while (result[i])
 	{
 		write(1, &result[i], 1);
 		i++;
@@ -232,7 +126,7 @@ void print_result(const char* result)
 
 void simple_printf(const char* format, ...)
 {
-	va_list args;
+	va_list	args;
 	int		i;
 	int		count;
 	char	f_specifier;
@@ -242,12 +136,16 @@ void simple_printf(const char* format, ...)
 	result = (char*)format;
 	count = count_args(format);
 	va_start(args, format);
-	while(i < count)
+	while (i < count)
 	{
 		f_specifier = get_specifier(format, i);
-		if(f_specifier == 's')
+		if (f_specifier == 's')
 		{
-			result = get_result(result, va_arg(args, char*));
+			result = put_string(result, va_arg(args, char*));
+		}
+		else if(f_specifier == 'i')
+		{
+			result = put_number(result, va_arg(args, int));
 		}
 		i++;
 	}
@@ -258,8 +156,12 @@ void simple_printf(const char* format, ...)
 
 int main()
 {
-	simple_printf("String: %s", "Goodbye");
-	// char *str = "Hello, world!";
-	// str = str + 7;
-	// printf("%s\n", str);
+	simple_printf("String: %s, my friends %s", "25", "12");
+
+	// printf("Test Case 1: Simple Integer\n");
+    // simple_printf("The number is: %i\n", 42);
+	// printf("\nTest Case 2: Simple String\n");
+    // simple_printf("Hello, %s!\n", "world");
+	// printf("\nTest Case 3: Multiple Integers\n");
+    // simple_printf("Numbers: %i, %i, and %i\n", 1, 2, 3);
 }
